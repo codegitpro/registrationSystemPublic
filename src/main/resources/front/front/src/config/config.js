b14c78c63c13486071ac1c548028d49c985aec7b
ynw <931708230@qq.com>
2025-05-10 22:55:01 +0800

    first commit

---
+++ src/main/resources/front/front/src/config/config.js
@@ -0,0 +1,17 @@
+export default {
+    baseUrl: 'http://localhost:8080/registrationSystem/',
+    indexNav: [
+        {
+            name: '首页',
+            url: '/index/home'
+        },
+        {
+            name: '考试信息',
+            url: '/index/kaoshixinxi'
+        },
+        {
+            name: '新闻资讯',
+            url: '/index/news'
+        },
+    ]
+}
